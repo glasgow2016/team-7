835f96f0c0724b973cb752eda4e1f13989312703
Andrew Smith <Andrew Smith>
2016-11-05 23:15:10 +0000

    Images accessable by everyone

--- avatar.py
+++ avatar.py
@@ -38,32 +38,32 @@ def page1():
     #avatars to click on
     b1 = Button(root,command = headphones)
     b1.pack(side=LEFT)
-    photoHP = PhotoImage(file="C:\\Users\\andyj\\Documents\\tkinter\\headphones.gif",width = 150,height = 300)
+    photoHP = PhotoImage(file="headphones.gif",width = 150,height = 300)
     b1.config(image = photoHP,compound = CENTER)
 
     b2 = Button(root,command = hiVis)
     b2.pack(side = LEFT)
-    photoHV = PhotoImage(file="C:\\Users\\andyj\\Documents\\tkinter\\hiVis.gif",width = 150,height = 300)
+    photoHV = PhotoImage(file="hiVis.gif",width = 150,height = 300)
     b2.config(image = photoHV,compound = CENTER)
 
     b3 = Button(root,command = phone)
     b3.pack(side = LEFT)
-    photoP = PhotoImage(file="C:\\Users\\andyj\\Documents\\tkinter\\phone.gif",width = 150,height = 300)
+    photoP = PhotoImage(file="phone.gif",width = 150,height = 300)
     b3.config(image = photoP,compound = CENTER)
 
     b4 = Button(root,command = hoodie)
     b4.pack(side = LEFT)
-    photoH = PhotoImage(file="C:\\Users\\andyj\\Documents\\tkinter\\hoodie.gif",width = 150,height = 300)
+    photoH = PhotoImage(file="hoodie.gif",width = 150,height = 300)
     b4.config(image = photoH,compound = CENTER)
 
     b5 = Button(root,command = cap)
     b5.pack(side = LEFT)
-    photoC = PhotoImage(file="C:\\Users\\andyj\\Documents\\tkinter\\cap.gif",width = 150,height = 300)
+    photoC = PhotoImage(file="cap.gif",width = 150,height = 300)
     b5.config(image = photoC,compound = CENTER)
 
     b6 = Button(root,command = elderly)
     b6.pack(side = LEFT)
-    photoE = PhotoImage(file="C:\\Users\\andyj\\Documents\\tkinter\\elderly.gif",width = 150,height = 300)
+    photoE = PhotoImage(file="elderly.gif",width = 150,height = 300)
     b6.config(image = photoE,compound = CENTER)
 
     bExit = Button(text = "Next")
@@ -89,4 +89,4 @@ def page1():
 
 
     root.mainloop()
-#page1()
+page1()
